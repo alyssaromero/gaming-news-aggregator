3fa7e5fd976beae070da0bf4dbeeb5286499787e
Alyssa Romero <alyssaromero@Alyssas-MacBook-Pro.local>
2019-08-14 18:14:31 -0500

    Deleted Gamasutra...Will Fix XPaths Later

--- gamingNewsAggregator/routes.py
+++ gamingNewsAggregator/routes.py
@@ -16,19 +16,6 @@ def home():
         command: scrapy crawl <nameOfSpider> -o spiders/<nameOfSpider>.json
         Wait for this command to finish, and read output to client
     """
-    """
-        should i just write all articles to single json file?
-        grab UTC time, and insert into single json file. Each time app is called,
-            check if currnt UTC is 6 hours ahead of previous UTC...
-            if True: delete json file, insert current UTC in JSON file and rerun spiders
-            if False: reuse old articles
-    """
-
-    """
-    spiders = ['gamasutra', 'gameInformer', 'gamesRadar', 'polygon']
-    for s in spiders:
-        subprocess.check_output(['scrapy', 'crawl', s, '-o', 'spiders/spider_files/'+s+'.json'])
-    """
     ### Base Case ... First Run ... Files DNE ###
     if (len(os.listdir("spiders/spider_files")) == 0):
         utc_now = pytz.utc.localize(datetime.datetime.utcnow())
@@ -44,10 +31,9 @@ def home():
              'Second': utc_now.second
              }
         )
-        #data["utc_old"].append(pytz.utc.localize(datetime.datetime.utcnow()))
         with open("spiders/spider_files/time.json", "w") as file:
             json.dump(data, file)
-        spiders = ['gamasutra', 'gameInformer', 'gamesRadar', 'polygon']
+        spiders = ['gameInformer', 'gamesRadar', 'polygon']
         for s in spiders:
             subprocess.check_output(['scrapy', 'crawl', s, '-o', 'spiders/spider_files/'+s+'.json'])
     else:
@@ -56,7 +42,7 @@ def home():
             jsontime = json.load(json_file)
             hour = jsontime['utc_old'][0]['Hour']
             if (utc_now.hour - hour >= 5):
-                spiders = ['gamasutra', 'gameInformer', 'gamesRadar', 'polygon']
+                spiders = ['gameInformer', 'gamesRadar', 'polygon']
                 ## Remove Previous JSON files and Update ##
                 for s in spiders:
                     os.remove('spiders/' + s + '.json')
@@ -81,11 +67,7 @@ def home():
                     json.dump(data, file)
 
     #os.remove("spiders/gamesRadar.json") #to remove json file...this works
-    """
-    gamasutra = []
-    with open('spiders/spider_files/gamasutra.json') as json_file:
-        data = json.load(json_file)
-        gamasutra.append(data)
+
     gameInformer = []
     with open('spiders/spider_files/gameInformer.json') as json_file:
         data = json.load(json_file)
@@ -98,9 +80,9 @@ def home():
     with open('spiders/spider_files/polygon.json') as json_file:
         data = json.load(json_file)
         polygon.append(data)
-    """
-    #return render_template("home.html", gamasutra=gamasutra, gameInformer=gameInformer, gamesRadar=gamesRadar, polygon=polygon, len=20)
-    return render_template("index.html", utc_now=utc_now, time=jsontime, hour=jsonhour)
+
+    return render_template("home.html", gameInformer=gameInformer, gamesRadar=gamesRadar, polygon=polygon, len=20)
+    #return render_template("index.html", utc_now=utc_now, time=jsontime, hour=jsonhour)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- gamingNewsAggregator/spiders/gamasutra.py
+++ gamingNewsAggregator/spiders/gamasutra.py
@@ -12,13 +12,16 @@ class gamasutra(scrapy.Spider):
         #articles = response.xpath("//div[@class='content_box_middle']/div[@class='feed_item']/span/span")
         #image = response.xpath("//div[@class='content_box_middle']/div[@class='feed_item']/a")
 
-        #for some reason, xpaths are not grabbing italized titles... fix this
+        #for a in art:
+            #a.css('a *::text').extract()
+
+        # Links are not consistent... some contain https// and some do not?
+        # Figure out way to append https// to those that don't start with it
         for art in articles:
             yield {
-                'headline': art.css('a::text').get(),
-                #'headline': art.xpath("//div[@class='feed_item']/span/span[@class='story_title']").css("a::text").get(),
-                'link': art.css("a::attr(href)").get()
-                #'link': art.xpath("//div[@class='feed_item']/span/span[@class='story_title']").css("a::attr(href)").get(),
+                #'headline': art.css('a::text').get() + art.xpath("//span[@class='story_title']/a/child::*//text()").get(),
+                'headline': art.css('a *::text').extract(), #Must Concatenate all Elements Occurring Before '&nbsp'
+                'link': art.css("a::attr(href)").get() #Link Works
                 #'image': art.css("img::attr(src)").get(),
                 #'image': art.xpath("//a/img[@class='thumbnail_left']").css("img::attr(src)").get()
             }
